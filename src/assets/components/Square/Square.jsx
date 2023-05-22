99d57eecd2298032e80771aa589fb692b54af517
nicolamberto <nicolaslamberto2840@gmail.com>
2023-05-22 18:11:07 -0300

    tictactoe game

---
+++ src/assets/components/Square/Square.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './Square.css';
+import classNames from "classnames";
+
+const Square = ({value, onClick, turn, winner})=> {
+
+    const handleClick = () =>{
+       (turn !== null && value === null) && onClick();
+    }
+
+    let squareClass = classNames({
+        square: true,
+        [`square--${value}`]: value !== null,
+        winner: winner
+    })
+
+    console.log(squareClass);
+
+    return (
+        <div className={squareClass} onClick={()=> handleClick()}>
+
+        </div>
+    )
+}
+
+export default Square;
